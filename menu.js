cbf54db03cc2f2883d439b0d736c99ee4bfc7a80
Anders N Kodehode <anders.kodehode@gmail.com>
2022-02-16 09:36:37 +0100

    added ripple effect on mouse click

--- menu.js
+++ menu.js
@@ -1,5 +1,27 @@
+//Mouse ripple click effect
+// const pulse = function() {
+//     const e = document.createElement('div');
+//     e.setAttribute('class', 'ripple'),
+//     document.body.appendChild(e),
+//     e.style.top = event.pageY + 'px',
+//     e.style.left = event.pageY + 'px',
+
+//     setTimeout(function() {
+//         document.body.removeChild(e)
+//     }, 1000)
+// };
+// document.addEventListener('click', pulse);
+window.addEventListener('click', function(e) {
+    const ripple = document.createElement('div');
+    ripple.className = 'ripple';
+    this.document.body.appendChild(ripple);
+    ripple.style.left = `${e.clientX}px`;
+    ripple.style.top = `${e.clientY}px`; 
+})
+// --------------------------------------------------
+
 // Indicator moves to active list item
-let list = document.querySelectorAll('.list');
+const list = document.querySelectorAll('.list');
 list.forEach((item) =>
 item.addEventListener('click', activeLink));
 
@@ -11,7 +33,6 @@ function activeLink(){
 // --------------------------------------------------
 
 // Play audio on keydown
-
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
@@ -21,3 +42,4 @@ window.addEventListener('keydown', function(e) {
     console.log(key);
     key.classList.add('playing')
 });
+// --------------------------------------------------
